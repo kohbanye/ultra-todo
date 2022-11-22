0c27b83fa7ab9075bc28ba3cec01faad65c4efd2
koh <kohbanye@gmail.com>
2022-11-23 02:29:39 +0900

    add config

---
+++ config/db.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func Init() {
+	c := viper.New()
+	c.SetConfigName("config")
+	c.SetConfigType("yaml")
+	c.AddConfigPath(".")
+	c.AddConfigPath("./config")
+
+	err := c.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		c.GetString("db.user"),
+		c.GetString("db.password"),
+		c.GetString("db.host"),
+		c.GetString("db.port"),
+		c.GetString("db.name"),
+	)
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
+
+func CloseDB() {
+	sqlDB, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	err = sqlDB.Close()
+	if err != nil {
+		panic(err)
+	}
+}
