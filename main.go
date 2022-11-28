a3f8de21f190b05611a0e2c26381bb2a2e77fc1c
koh <kohbanye@gmail.com>
2022-11-29 01:03:37 +0900

    add Authorization to AllowHeaders

--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 	"github.com/kohbanye/ultra-todo/config"
 	"github.com/kohbanye/ultra-todo/router"
 )
 
 func main() {
-	r := router.Init()
+	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Authorization"},
+	}))
+
+	r = router.Init(r)
 	config.Init()
 
 	r.Run()
--- router/router.go
+++ router/router.go
@@ -1,20 +1,11 @@
 package router
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kohbanye/ultra-todo/controller"
 )
 
-func Init() *gin.Engine {
-	r := gin.Default()
-
-	r.Use(cors.New(cors.Config{
-		AllowOrigins: []string{"*"},
-		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders: []string{"Origin", "Content-Type"},
-	}))
-
+func Init(r *gin.Engine) *gin.Engine {
 	authMiddleware, err := controller.AuthMiddleware()
 	if err != nil {
 		panic(err)
