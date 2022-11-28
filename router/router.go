ff5e4bd1a54874314aadfb316b4f2ff87902690e
koh <kohbanye@gmail.com>
2022-11-28 14:28:47 +0900

    add cors config

--- router/router.go
+++ router/router.go
@@ -1,12 +1,19 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kohbanye/ultra-todo/controller"
 )
 
 func Init() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders: []string{"Origin", "Content-Type"},
+	}))
 
 	r.GET("/tasks/:id", controller.GetTask())
 	r.POST("/tasks", controller.CreateTask())
