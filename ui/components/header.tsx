292c306cadcf31e540efbcdf04e925da77ac17f1
koh <kohbanye@gmail.com>
2022-11-29 14:51:26 +0900

    fix: use Link

--- ui/components/header.tsx
+++ ui/components/header.tsx
@@ -1,25 +1,17 @@
 import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
 
 import styles from 'styles/Header.module.scss'
 
 const Header = () => {
-  const router = useRouter()
-
   return (
     <div className={styles.wrapper}>
       <Link href="/">
         <div className={styles.logo}>Ultra ToDo</div>
       </Link>
-      <div
-        className={styles.user}
-        onClick={() => {
-          router.push('/user')
-        }}
-      >
+      <Link href="/user" className={styles.user}>
         <AccountCircleIcon className={styles.icon} />
-      </div>
+      </Link>
     </div>
   )
 }
